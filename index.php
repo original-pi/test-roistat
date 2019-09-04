345f896ddf67d33ba0864a93aa97f5c4cdc9d218
Pavel Ivanichko <43095840+original-pi@users.noreply.github.com>
2019-09-04 18:50:11 +0300

    Create index.php
    new function

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+function revertPunctuationMarks($string){
+    $res = "";
+    return $res;
+}
+
+$result = revertPunctuationMarks("Привет! Как твои дела?");
+echo $result; // Привет? Как твои дела!
